ef5ac988abe0f4a7bee57517bfbadfc7b5bd4311
Ivan K <58504325+kapitan-karablya@users.noreply.github.com>
2021-05-15 02:53:01 +0500

    Добавил боковое меню

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from "react";
 import Authorization from "./components/Authorization";
 import Registration from "./components/Registration";
 import LoginHeader from "./components/LoginHeader";
+import Sidebar from "./components/Sidebar";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Redirect from "react-router-dom/es/Redirect";
+
 
 
 function App() {
     return (
         <div className="body">
-            <LoginHeader/>
-            <Registration/>
+            <Sidebar/>
         </div>
     );
 }
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import './style.css'
+import {BrowserRouter, NavLink} from "react-router-dom";
+
+import projects from '../../icons/projects.svg'
+import current_project from '../../icons/current_project.svg'
+import tasks from '../../icons/tasks.svg'
+import teams from '../../icons/teams.svg'
+import logo from '../../icons/logo.svg'
+
+
+
+
+const sidebarItemsOptions = [
+    {text: 'Проекты', image: projects, href: '/home'},
+    {text: 'Текущий проект', image: current_project, href: '/project'},
+    {text: 'Задачи', image: tasks, href: '/tasks'},
+    {text: 'Команды', image: teams, href: '/teams'},
+];
+
+class Sidebar extends React.Component {
+    render() {
+        const sidebarItems = sidebarItemsOptions.map(item => <SidebarItem item={item}
+            /*createClickHandler={createClickHandler}*//>);
+        return (
+            <BrowserRouter>
+                <div className='sidebar'>
+                    <div className='sidebar-item sidebar-logo'>
+                        <div className='sidebar-logo-image'><img src={logo} alt="logo"/></div>
+                        <div className='sidebar-item-text sidebar-logo-text'>rogenager.io</div>
+                    </div>
+                    {sidebarItems}
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+function SidebarItem({/*createClickHandler, */item}) {
+    /*const clickHandler = createClickHandler(item.text);*/
+
+    return (
+        <div className='sidebar-item'/* onClick={clickHandler}*/>
+            <NavLink className='sidebar-item' activeClassName="active" to={item.href}>
+                <div className='sidebar-image'><img src={item.image} alt={item.text}/></div>
+                <div className='sidebar-item-text'><span>{item.text}</span></div>
+            </NavLink>
+        </div>
+    )
+}
+
+export default Sidebar
